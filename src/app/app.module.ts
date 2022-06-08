26c233aeb7f86aef288ab26f844d9740f14d214b
Tomasz Boguski <tomasz.boguski@softiq.pl>
2022-06-08 13:02:24 +0200

    Home & NotFound pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
