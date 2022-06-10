f766a3174fc3b29a2f44eb2a65b1fe9b2a8d7135
Tomasz Boguski <tomasz.boguski@softiq.pl>
2022-06-10 15:56:43 +0200

    Added flag in menu component

--- src/app/core/components/menu/menu.component.ts
+++ src/app/core/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/user/services/user.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+
   }
 
+  checkIsLogged() : boolean {
+    return this.userService.isLogged;
+  }
 }
